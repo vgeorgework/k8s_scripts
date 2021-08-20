37768bebf9d819f2b9afc69946153689542ab00e
Vineeth george <39851362+vgeorgework@users.noreply.github.com>
2021-08-20 19:57:14 +0530

    final commit
    Added additional functionality.

--- Jobs status script/jobstatus.sh
+++ Jobs status script/jobstatus.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
-# Usage  #./looper.sh <namespacename>
-# while : # for inifinity loop
-# do
-echo "Fetching details for namespace $1"
+# chmod 777 jobstatus.sh
+# Usage  #./jobstatus.sh <namespacename>
+
+echo "Fetching jobs from namespace $1"
 for res in $(kubectl get --no-headers=true jobs -n $1 | awk '{print $1}');do # job loop
     status=$(kubectl get job $res -n demo -o jsonpath='{.status.succeeded}')
-    if [[ $status -eq '1' ]]
+    if [[ $status -eq '1' ]] # Pass condition
         then
         echo "Job Name: $res                Status: Success"
         # exit 0
-    elif [[ $status -eq '0' ]]
+    elif [[ $status -eq '0' ]] # Failed condition
         then
         echo "Job Name: $res                Status: Failed"
+        # exit 0 # will break and exit from the loop.
+        # while :
+        # do
+        #     status=$(kubectl get job $res -n demo -o jsonpath='{.status.succeeded}')
+        #     if [[ $status -eq '1' ]]
+        #         then
+        #         echo "Job Name: $res                Status: Success"
+        #     fi
+        # done    
+        
     fi
 done
-# done
